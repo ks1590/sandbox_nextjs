6c0305c6f9709f7003cab5499e958891aeb42a48
ks1590 <69412467+ks1590@users.noreply.github.com>
2023-05-05 19:20:10 +0900

    delete: imported Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,12 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import type { LayoutProps } from '@vercel/examples-ui/layout';
 import '../styles/global.css';
-// import Layout from '../components/layout';
-
-// import { Layout, getLayout } from '@vercel/examples-ui';
 
 import '@vercel/examples-ui/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
-	// const Layout = getLayout<LayoutProps>(Component)
 
 	return (
-		// <Layout
-		//   title="ai-chatgpt"
-		//   path="solutions/ai-chatgpt"
-		//   description="ai-chatgpt"
-		// >
-		//   <Component {...pageProps} />
-		//   <Analytics />
-		// </Layout>
-    // <Layout>
       <div className='main-container'>
         <Component {...pageProps} />      
       </div>
